09b4894754830b16fa8c950b4a95f6da8c039323
Devon Nelson <s809569@sbuniv.edu>
2020-10-24 18:17:31 -0500

    troubleshooting login

--- web/functions/vote.php
+++ web/functions/vote.php
@@ -4,7 +4,7 @@
 		//$_SESSION['username'] = strtolower(stringClean($_POST['username']));
 		$result = queryHandler("SELECT * FROM auth WHERE webcode='" . password_hash($_POST['username'],PASSWORD_DEFAULT) . "'");
 		$row = $result->fetch_array();
-		if($result->num_rows > 0 && password_verify($_POST['password'],$row['password'])){
+		if(password_verify($_POST['password'],$row['password'])){
 			$_SESSION['loggedin'] = "true";
 			exit(header('Location: ballot.php'));
 		}
