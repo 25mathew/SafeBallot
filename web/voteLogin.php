48155262174367e93defc9f9da5034fdd8e65c8f
Slate Rehm <s515940@sbuniv.edu>
2020-10-24 13:59:26 -0500

    login page

--- web/voteLogin.php
+++ web/voteLogin.php
@@ -7,9 +7,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
-
-
-<body id="flagBackground" class="verticalContent">
+<!--<body id="flagBackground" class="verticalContent">
 
     <div id="centerPanel" class="CenterContent">
         <h3 id="whiteText" class="contentPanelH1 title">Vote</h3>
@@ -20,10 +18,39 @@
             <label for="password">Password</label><br>
             <input type="password" id="tinput" name="password"> <br>
 
-            <!--<a href="ballot.html" class="button">Submit</a>-->
+            <a href="ballot.html" class="button">Submit</a>
 			<input type="submit" value="Submit" class="button">
             <a href="new-password.html" class="button">Lost your password?</a>
         </form>
+    </div>
+</body>-->
+
+<body>
+    <div div class="panel" id="leftPanel">
+        <h4 class="leftPanelH4">SafeBallot</h4>
+        <h1 class:"leftPanelH1">Login</h1>
+    </div>
+
+    <div class="panel" id="contentPanel">
+        <div class="CenterContent">
+            <h1 class="contentPanel" style="text-align:center">Log in</h1>
+
+            <form action="functions/vote.php" method="post" style="display:block;">
+                <label for="username">Username</label><br>
+                <input type="text" id="tinput" name="username"><br>
+
+                <label for="password">Password</label><br>
+                <input type="password" id="tinput" name="password"> <br>
+
+                <!--<a href="ballot.html" class="button">Submit</a>-->
+                <input type="submit" value="Submit" class="button">
+                <a href="issueNewPassword.html" class="button">Lost your password?</a>
+            </form>
+        </div>
+    </div>
+
+    <div class="panel" id="rightPanel">
+
     </div>
 </body>
 </html>
