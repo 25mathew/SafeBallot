d2b1446d6263478929ff321fef8ded9ef7d67732
Slate Rehm <s515940@sbuniv.edu>
2020-10-24 16:54:03 -0500

    added covid page and home link

--- web/voteLogin.php
+++ web/voteLogin.php
@@ -7,33 +7,15 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
-<!--<body id="flagBackground" class="verticalContent">
-
-    <div id="centerPanel" class="CenterContent">
-        <h3 id="whiteText" class="contentPanelH1 title">Vote</h3>
-        <form action="functions/vote.php" method="post" style="display:block">
-            <label for="username">Username</label><br>
-            <input type="text" id="tinput" name="username"><br>
-
-            <label for="password">Password</label><br>
-            <input type="password" id="tinput" name="password"> <br>
-
-            <a href="ballot.html" class="button">Submit</a>
-			<input type="submit" value="Submit" class="button">
-            <a href="new-password.html" class="button">Lost your password?</a>
-        </form>
-    </div>
-</body>-->
-
 <body>
     <div div class="panel" id="leftPanel">
-        <h4 class="leftPanelH4">SafeBallot</h4>
-        <h1 class:"leftPanelH1">Login</h1>
+        <a href="index.html"><h4 class="leftPanelH4">SafeBallot</h4></a>
+        <h1 class="leftPanelH1">Login</h1>
     </div>
 
     <div class="panel" id="contentPanel">
         <div class="CenterContent">
-            <h1 class="contentPanel" style="text-align:center">Log in</h1>
+            <h1 class="contentPanel" style="text-align:center">Login</h1>
 
             <form action="functions/vote.php" method="post" style="display:block;">
                 <label for="username">Username</label><br>
