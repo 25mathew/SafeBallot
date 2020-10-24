26349315362a3cb3dcfb2399ff4280f1000d424c
DevonN8650 <41879272+DevonN8650@users.noreply.github.com>
2020-10-24 15:22:40 -0500

    Update register.php

--- web/functions/register.php
+++ web/functions/register.php
@@ -14,5 +14,35 @@
 			//implement later
 		}
 	}
+	function validationCodeHandler($username){
+		$result = queryHandler("SELECT * FROM auth");
+		$code = createRandomCode();
+		while($row = $result->fetch_array()){ #Checks for duplicate, pre-existing code
+				if($row['activation_code'] == $code){
+					return validationCodeHandler($username);
+				}
+		}
+		$update_statement = queryHandler("UPDATE auth SET activation_code = '" . $code . "' WHERE username = '" . $username . "'");
+
+		return $code;
+
+	}
+	function createRandomCode() {
+
+    		$chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ023456789";
+    srand((double)microtime()*1000000);
+    $i = 0;
+    $pass = '' ;
+
+    while($i <= 25){
+        $num = rand() % 33;
+        $tmp = substr($chars, $num, 1);
+        $pass = $pass . $tmp;
+        $i++;
+    }
+
+    return $pass;
+
+}
 	
 ?>
