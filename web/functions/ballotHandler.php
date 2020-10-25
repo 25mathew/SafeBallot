c8022ee84312e093d3e505d47756a9e88ddd0f31
Devon Nelson <s809569@sbuniv.edu>
2020-10-25 13:44:14 -0500

    test

--- web/functions/ballotHandler.php
+++ web/functions/ballotHandler.php
@@ -1,13 +1,14 @@
 <?php
 	session_start();
 	$result = queryHandler("SELECT * FROM ballot WHERE id = 7");
-	$row = $result->fetch_array();
-	if($row['voted'] == "false"){
-		$result = queryHandler('UPDATE ballot SET voted = "true" WHERE id = ' . $_SESSION['ID']);
-		$_SESSION['vote_result'] = "Congratulations! Your vote was received!";
-	}
-	else{
-		$_SESSION['vote_result'] = "You have already voted!";
-	}
-	header('Location: ../voteConfirmation.php');
+	#$row = $result->fetch_array();
+	#if($row['voted'] == "false"){
+	#	$result = queryHandler('UPDATE ballot SET voted = "true" WHERE id = ' . $_SESSION['ID']);
+	#	$_SESSION['vote_result'] = "Congratulations! Your vote was received!";
+	#}
+	#else{
+	#	$_SESSION['vote_result'] = "You have already voted!";
+	#}
+	#header('Location: ../voteConfirmation.php');
+	echo "test";
 ?>
