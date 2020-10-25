8251f52bbc023f139998c506c9e269e5ca4c6f2d
Devon Nelson <s809569@sbuniv.edu>
2020-10-25 09:59:48 -0500

    syntax issue

--- web/functions/register.php
+++ web/functions/register.php
@@ -7,7 +7,7 @@
 			$result = queryHandler("INSERT INTO pii (ID,SSN,DLN) VALUES (" . $_SESSION['ID'] . ",'" . password_hash($SSN,PASSWORD_DEFAULT) . "','" . password_hash($DLN,PASSWORD_DEFAULT) . "')");
 			$result = queryHandler("INSERT INTO ballot (ID, voted) VALUES (" . $_SESSION['ID'] . ",'false')");
 			$result = queryHandler("INSERT INTO auth (ID,webcode,mailcode) VALUES (" . $_SESSION['ID'] . ",'" . password_hash($_SESSION['webcode'],PASSWORD_DEFAULT) . "','" . password_hash($_SESSION['mailcode'],PASSWORD_DEFAULT) . "')");
-			echo "$_SESSION['ID'];
+			echo $_SESSION['ID'];
 			$_SESSION['ID'] = null;
 			echo "\nwebcode: " . $_SESSION['webcode']; 
 			echo "\nmailcode: " . $_SESSION['mailcode'];; 
