e818d995af28f99fc3b0d25911df41b8d48123c9
Devon Nelson <s809569@sbuniv.edu>
2020-10-25 13:27:58 -0500

    ballot troubleshooting

--- web/functions/ballotHandler.php
+++ web/functions/ballotHandler.php
@@ -1,15 +1,16 @@
 <?php
 	if(issset($_POST['ballot'])){
 		$result = queryHandler("SELECT * FROM ballot WHERE id = " . $_SESSION['ID']);
-		$row = $result->fetch_array();
-		if($row['voted'] == "false"){
-			$result = queryHandler('UPDATE ballot SET voted = "true" WHERE id = ' . $_SESSION['ID']);
-			$_SESSION['vote_result'] = "Congratulations! Your vote was received!";
-		}
-		else{
-			$_SESSION['vote_result'] = "You have already voted!";
-		}
-		header('Location: ../voteConfirmation.php');
+		#$row = $result->fetch_array();
+		#if($row['voted'] == "false"){
+		#	$result = queryHandler('UPDATE ballot SET voted = "true" WHERE id = ' . $_SESSION['ID']);
+		#	$_SESSION['vote_result'] = "Congratulations! Your vote was received!";
+		#}
+		#else{
+		#	$_SESSION['vote_result'] = "You have already voted!";
+		#}
+		#header('Location: ../voteConfirmation.php');
+		echo "hello sir";
 	}
 
 
