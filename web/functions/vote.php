7b6232d085f003e244c527335e23a4db6f0ed0e7
Devon Nelson <s809569@sbuniv.edu>
2020-10-25 13:23:56 -0500

    ballot handling

--- web/functions/vote.php
+++ web/functions/vote.php
@@ -3,7 +3,7 @@
 	if(isset($_POST['username']) && isset($_POST['password'])){
 		if(dualHashedQueryHandler($_POST['username'],$_POST['password'],'webcode','mailcode','SELECT * FROM auth')){
 			$_SESSION['loggedin'] = "true";
-			header('Location: ballot.php');
+			header('Location: ../vote.html');
 		}
 		else{
 			$_SESSION['error'] = true;
