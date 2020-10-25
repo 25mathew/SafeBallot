bc2aef10ce957c752a5217dcc4bff45181454437
Slate Rehm <s515940@sbuniv.edu>
2020-10-25 17:29:39 -0500

    vote partial

--- web/scripts/vote.js
+++ web/scripts/vote.js
@@ -8,3 +8,112 @@ $(".pres").click(function () {
   
      $(this).removeClass("not-selected");
  });
+
+ $(".gov").click(function () {
+
+     console.log("clicked");
+     $(".gov").removeClass("selected");
+     
+     $(this).addClass("selected");
+     $(".gov").addClass("not-selected");
+  
+     $(this).removeClass("not-selected");
+ });
+
+ $(".house").click(function () {
+
+     console.log("clicked");
+     $(".house").removeClass("selected");
+     
+     $(this).addClass("selected");
+     $(".house").addClass("not-selected");
+  
+     $(this).removeClass("not-selected");
+ });
+
+ $(".lieu").click(function () {
+
+     console.log("clicked");
+     $(".lieu").removeClass("selected");
+     
+     $(this).addClass("selected");
+     $(".lieu").addClass("not-selected");
+  
+     $(this).removeClass("not-selected");
+ });
+
+ $(".att").click(function () {
+
+     console.log("clicked");
+     $(".att").removeClass("selected");
+     
+     $(this).addClass("selected");
+     $(".att").addClass("not-selected");
+  
+     $(this).removeClass("not-selected");
+ });
+
+ $(".sec").click(function () {
+
+     console.log("clicked");
+     $(".sec").removeClass("selected");
+     
+     $(this).addClass("selected");
+     $(".sec").addClass("not-selected");
+  
+     $(this).removeClass("not-selected");
+ });
+
+ $(".sup").click(function () {
+
+     console.log("clicked");
+     $(".sup").removeClass("selected");
+     
+     $(this).addClass("selected");
+     $(".sup").addClass("not-selected");
+  
+     $(this).removeClass("not-selected");
+ });
+
+ $(".dis").click(function () {
+
+     console.log("clicked");
+     $(".dis").removeClass("selected");
+     
+     $(this).addClass("selected");
+     $(".dis").addClass("not-selected");
+  
+     $(this).removeClass("not-selected");
+ });
+ $(".app").click(function () {
+
+     console.log("clicked");
+     $(".app").removeClass("selected");
+     
+     $(this).addClass("selected");
+     $(".app").addClass("not-selected");
+  
+     $(this).removeClass("not-selected");
+ });
+
+ $(".trea").click(function () {
+
+     console.log("clicked");
+     $(".trea").removeClass("selected");
+     
+     $(this).addClass("selected");
+     $(".trea").addClass("not-selected");
+  
+     $(this).removeClass("not-selected");
+ });
+
+ $(".pres").click(function () {
+
+     console.log("clicked");
+     $(".pres").removeClass("selected");
+     
+     $(this).addClass("selected");
+     $(".pres").addClass("not-selected");
+  
+     $(this).removeClass("not-selected");
+ });
